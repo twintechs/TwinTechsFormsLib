db4b99dfc78f5d72207d6c95ed822a4a846ca0dd
George Cook <george.cook@twintechs.com>
2015-05-30 21:54:39 -0500

    updated options page

--- TwinTechsForms/TwinTechsFormsExample/TwinTechs/Example/GridView/GridViewOptions.xaml.cs
+++ TwinTechsForms/TwinTechsFormsExample/TwinTechs/Example/GridView/GridViewOptions.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using System.Collections.ObjectModel;
 
 namespace TwinTechs.Example.GridView
 {
@@ -13,10 +14,36 @@ namespace TwinTechs.Example.GridView
 			MediaItemsGridView.ItemsSource = DataProvider.GetMediaItems ();
 		}
 
-		void OnToggleCentered (object sender, object e)
+		void OnToggleAutoPad1 (object sender, object e)
 		{
 			MediaItemsGridView.IsContentCentered = !MediaItemsGridView.IsContentCentered;
 			CenteredMode.IsEnabled = MediaItemsGridView.IsContentCentered;
+
+			if (MediaItemsGridView.IsContentCentered) {
+				var items = DataProvider.GetMediaItems ();
+				var newProvider = new ObservableCollection<MediaItem> ();
+				newProvider.Add (items [0]);
+				newProvider.Add (items [1]);
+				MediaItemsGridView.ItemsSource = newProvider;
+			} else {
+				MediaItemsGridView.ItemsSource = DataProvider.GetMediaItems ();
+			}
+
+		}
+
+		void OnToggleAutoPad2 (object sender, object e)
+		{
+			MediaItemsGridView.IsContentCentered = !MediaItemsGridView.IsContentCentered;
+			CenteredMode.IsEnabled = MediaItemsGridView.IsContentCentered;
+			MediaItemsGridView.ItemsSource = DataProvider.GetMediaItems ();
+
+			if (!MediaItemsGridView.IsContentCentered) {
+				MediaItemsGridView.ContentPaddingLeft = 20;
+				MediaItemsGridView.ContentPaddingRight = 40;
+				MediaItemsGridView.RowSpacing = 3;
+				MediaItemsGridView.ColumnSpacing = 2;
+			}
+
 		}
 
 		void OnToggleCenteredMode (object sender, object e)
