71750f2936ef2fcbe88f435b6bda89aa6f6d97ea
George Cook <george.cook@twintechs.com>
2015-06-01 07:38:49 -0500

    reverted to xamarin forms 1.4.2

--- TwinTechsForms/Droid/Resources/Resource.designer.cs
+++ TwinTechsForms/Droid/Resources/Resource.designer.cs
@@ -26,8 +26,6 @@ namespace TwinTechsFormsExample.Droid
 		
 		public static void UpdateIdValues()
 		{
-			global::Xamarin.Forms.Platform.Resource.String.ApplicationName = global::TwinTechsFormsExample.Droid.Resource.String.ApplicationName;
-			global::Xamarin.Forms.Platform.Resource.String.Hello = global::TwinTechsFormsExample.Droid.Resource.String.Hello;
 			global::TwinTechsLib.Droid.Resource.String.ApplicationName = global::TwinTechsFormsExample.Droid.Resource.String.ApplicationName;
 			global::TwinTechsLib.Droid.Resource.String.Hello = global::TwinTechsFormsExample.Droid.Resource.String.Hello;
 			global::TwinTechsLib.Droid.Resource.String.library_name = global::TwinTechsFormsExample.Droid.Resource.String.library_name;
@@ -64,12 +62,6 @@ namespace TwinTechsFormsExample.Droid
 		
 		public partial class String
 		{
-			
-			// aapt resource value: 0x7f030002
-			public const int ApplicationName = 2130903042;
-			
-			// aapt resource value: 0x7f030001
-			public const int Hello = 2130903041;
 			
 			// aapt resource value: 0x7f030000
 			public const int library_name = 2130903040;
