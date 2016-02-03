948da438c9aaa61150edeb8b153a7a9c23296e32
Adam Patridge <patridge@gmail.com>
2016-02-02 20:29:27 -0700

    Added EnhancedWebView/-Renderer for Android.

---
+++ TwinTechsForms/TwinTechsForms.Core/TwinTechs/Controls/EnhancedWebView.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace TwinTechs.Controls
+{
+	/// <summary>
+	/// Just a subclass of WebView used to target with TwinTechs.Droid.EnhancedWebViewRenderer.
+	/// </summary>
+	public class EnhancedWebView : WebView
+	{
+	}
+}
